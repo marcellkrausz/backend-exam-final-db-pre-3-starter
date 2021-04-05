70a13c9576613e503d1f01cc37f01c023b4348df
marcellkrausz <krauszmaarcell@gmail.com>
2021-04-05 13:49:45 +0200

    add StudentsScores class

--- src/main/java/com/codecool/student_scores/StudentScores.java
+++ src/main/java/com/codecool/student_scores/StudentScores.java
@@ -1,5 +1,60 @@
 package com.codecool.student_scores;
 
+import java.sql.*;
+
 public class StudentScores {
-    
+    private final String URL;
+    private final String USER;
+    private final String PASSWORD;
+    private final Connection CONN;
+
+    public StudentScores(String URL, String USER, String PASSWORD) {
+        this.URL = URL;
+        this.USER = USER;
+        this.PASSWORD = PASSWORD;
+        this.CONN = getConnection();
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Database not reachable.");
+        }
+        return connection;
+    }
+    public String getCityWithHighestScore() {
+        try (this.CONN) {
+            String query = "SELECT city, SUM(score) as highest_score FROM student_scores " +
+                    "GROUP BY city " +
+                    "ORDER BY highest_score DESC LIMIT 1";
+            PreparedStatement ps = CONN.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+
+    public String getMostActiveStudent() {
+        try (this.CONN) {
+            String query = "SELECT student_name, COUNT(score) as most_scores FROM student_scores " +
+                    "GROUP BY student_name " +
+                    "ORDER BY most_scores DESC LIMIT 1";
+            PreparedStatement ps = CONN.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getString(1);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
 }
